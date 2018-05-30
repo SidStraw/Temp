9f479cfc407c8b957a1016529e05919c49e578d7
Half-Straw <j80304xz@gmail.com>
2018-05-30 19:02:58 +0800

    新增錨點滑動

--- function.js
+++ function.js
@@ -1,4 +1,14 @@
 $(document).ready(function () {
-    $('img[alt*="載入中"]').hide();
-    $('img[src*="_card_"]').addClass("addBorderRadius");
+    $('img[alt*="載入中"]').hide();//隱藏讀取轉圈
+    $('img[src*="_card_"]').addClass("addBorderRadius");//古法圖片動態
+
+
+    $('a[href*=#]:not([href=#])').click(function () {
+        var target = $(this.hash);
+        $('html,body').animate({
+            scrollTop: target.offset().top
+        }, 1000);
+        return false;
+    });// 錨點滑動
+
 })
