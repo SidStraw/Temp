9d1d5e613f0ba75ae92c9df4e73896fcbac948bc
Half-Straw <j80304xz@gmail.com>
2018-07-07 20:51:21 +0800

    更改「價格如設定」

--- function.js
+++ function.js
@@ -1,4 +1,5 @@
 jQuery(document).ready(function () {//不可以用＄
+    jQuery('p.price-as-configured span.price-label').text('售價：');//更改「價格如設定」
     jQuery('img[alt*="載入中"]').hide();//隱藏讀取轉圈
     jQuery('img[src*="_card_"]').addClass("addBorderRadius");//古法圖片動態
 
